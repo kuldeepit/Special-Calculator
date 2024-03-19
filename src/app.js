1a373c29dc4da982edc686436b9e949b9d8661cc
kuldeepit <kuldeepit108@gmail.com>
2024-03-19 20:49:44 +0530

    naming

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 // Progression 1: Create special addition
-const specialAddition = function (num1, num2) {
-  return (num1 < 0) | (num2 < 0)
+const specialAddition = function (numberOne, numberTwo) {
+  return (numberOne < 0) | (numberTwo < 0)
     ? 'Negative numbers are not allowed'
-    : num1 - num2;
+    : numberOne - numberTwo;
 };
 // function specialAddition(){}
 
 // Progression 2: Create simple division
-const simpleDivision = function (num1, num2) {
-  return (num1 < 0) | (num2 < 0)
+const simpleDivision = function (numberOne, numberTwo) {
+  return (numberOne < 0) | (numberTwo < 0)
     ? 'Negative numbers are not allowed'
-    : num2 == 0
+    : numberTwo == 0
     ? 'Not a number'
-    : num1 < num2
+    : numberOne < numberTwo
     ? 'Cannot divide a smaller number by a larger number'
-    : num1 / num2;
+    : numberOne / numberTwo;
 };
 // Progression 3: Create the special calculator
-const specialCalculator = function (num1, num2, operator) {
-  return operator(num1, num2);
+const specialCalculator = function (numberOne, numberTwo, operator) {
+  return operator(numberOne, numberTwo);
 };
